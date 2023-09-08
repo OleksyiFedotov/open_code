c727a2b7be3ddab9b6c1145feff26086bfd7c0b9
FoDizStudio <info.ycc.ua@gmail.com>
2023-09-08 11:39:52 +0300

    +

--- admin/controllers/ArticlesController.php
+++ admin/controllers/ArticlesController.php
@@ -1,100 +1,152 @@
 <?php
 
-namespace app\controllers;
+namespace app\modules\auth\controllers;
 
-use app\components\I18;
-use app\components\S22;
-use app\models\Article;
-use app\models\CallForm;
-use app\models\Category;
-use app\models\MessageForm;
+use app\components\Roles;
+use app\models\ArticleEdit;
 use Yii;
-use app\models\ImageUpload;
-use yii\base\ErrorException;
-use yii\filters\AccessControl;
-use yii\helpers\Url;
+use app\models\Article;
+use app\models\ArticleSearch;
+use app\modules\auth\models\User;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
-use yii\web\UploadedFile;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use app\models\ImageUpload;
+use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
-use yii\data\Pagination;
+use yii\web\UploadedFile;
+use app\modules\auth\controllers;
 
-class ArticlesController extends Controller
+/**
+ * ArticleController implements the CRUD actions for Article model.
+ */
+class ArticleController extends Controller
 {
     /**
-     * {@inheritdoc}
+     * @inheritDoc
      */
-   public function actionAll()
+    public function behaviors()
     {
-        try{
-            $id=Yii::$app->request->get('id');
-            if(!empty($id))
-            {
-                $page = Category::findOne($id);
-                $urlstart=$page->url;;
-                $url = S22::formatter_url($urlstart);
-            }else
-            {
-                $urlstart=Yii::$app->request->url;
-                $url = S22::formatter_url($urlstart);
-                $page=Category::find()->where(['url'=>$url])->select(['*'])->one();
-            }
-            $catid = !empty($page->id) ? $page->id : $id;
-            $title=I18::decode($page->title);
-            $this->setMeta($page->title, !empty($page->keywords) ? $page->keywords : $page->title,!empty($page->description) ? I18::decode($page->description): $page->title, $page->meta);
-            $query = Article::find()->where(['status' => true])->orderBy(['id'=>SORT_DESC]);
-            $countQuery = clone $query;
-            $pages = new Pagination(['totalCount'=>$countQuery->count(), 'pageSize'=>12]);
-            $allArticles=$query->offset($pages->offset)->limit($pages->limit)->all();
-            return $this->render('all', compact(['catid', 'url', 'title', 'allArticles', 'pages']));
-        }catch (ErrorException $e) {
-            return $this->redirect(['site/errors']);
-}
-
+        // it is code here 
     }
 
-    public function actionArticle()
+    /**
+     * Lists all Article models.
+     *
+     * @return string
+     */
+    public function actionIndex()
     {
-        try{
-            $id = Yii::$app->request->get('id');
-            $categoryId = Yii::$app->request->get('catid');
-            $category=Category::findOne($categoryId);
-            //all article
-            $article=Article::findOne($id);
-            $similarArticles = S22::Article();
-            $title=I18::decode($article->title);
-            $this->setMeta(I18::decode($article->title), !empty($article->keywords) ? $article->keywords : '', I18::decode($article->content), $article->meta);
-            //viewin
-            $article->viewedCounter();
-            $messageForm = new MessageForm();
-            if(Yii::$app->request->isPost and !empty($messageForm)) {
-                if ($messageForm->load(Yii::$app->request->post())) {
-                    $messageForm->validate();
-                    Yii::$app->mailer->compose('layouts/message', compact(['messageForm']))
-                        ->setFrom(['info@eservice.org.ua' => 'e-service'])
-                        ->setTo([Yii::$app->params['adminEmail']])
-                        ->setSubject('Клієнт підписався на листування.')
-                        ->send();
-                    Yii::$app->session->setFlash('success', "Ваc підписано на розсилання!");
-                    return $this->redirect(['articles/article', 'id' => $article->id, 'slug' =>$article->keywords, 'catid' => $categoryId]);
-                }else{
-                        return $this->redirect(['site/errors']);
-                    }
-            }else {
-                return $this->render('article', compact(['similarArticles', 'id', 'categoryId', 'category', 'title', 'article', 'messageForm']));
-            }
-        }catch (ErrorException $e) {
-            return $this->redirect(['site/errors']);
+        $searchModel = new ArticleSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Article model.
+     * @param int $id ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Article model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new ArticleEdit();
+
+        if ($model->load(Yii::$app->request->post())) {
+            $model->user_id = Yii::$app->user->id;
+            $model->encodeI18n();
+            if ($model->save())
+                return $this->redirect(['view', 'id' => $model->id]);
         }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
     }
 
-    protected function setMeta($title= null, $keywords = null, $content=null, $meta=null)
+    /**
+     * Updates an existing Article model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
     {
-        $this->view->title=I18::decode($title);
-        $this->view->registerMetaTag(['name' => 'keywords', 'content' =>"$keywords"]);
-        $this->view->registerMetaTag(['name' => 'content', 'content' =>"$content"]);
-        $this->view->registerMetaTag(['name' => 'robots', 'content' =>"$meta"]);
+        $model = ArticleEdit::findOne($id);
+
+        $model->setAttributes($this->findModel($id)->getAttributes());
+        $model->decodeI18n();
+        if ($model->load(Yii::$app->request->post())) {
+            $model->encodeI18n();
+            if ($model->save())
+                return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
     }
+
+    /**
+     * Deletes an existing Article model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Article model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return Article the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Article::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+
+    public function actionSetImage($id)
+    {
+        $model = new ImageUpload;
+        if (Yii::$app->request->isPost) {
+            $article = $this->findModel($id);
+            $file = UploadedFile::getInstance($model, 'image');
+            if ($article->saveImage($model->uploadFile($file, $article->image))) {
+                return $this->redirect(['view', 'id' => $article->id]);
+            }
+        }
+        return $this->render('image', ['model' => $model]);
+    }
+
 }
 
